b92ec14235bbbb10bda18c6407dcf375e50c84d1
Alex Kallaur <alex.kallaur@snowflake.com>
2025-01-20 14:25:15 +0100

    Day 28: Fix issues and update deprecated methods
    - Replace deprecated `st.experimental_memo` with `st.cache_data`
    - Correct indentation issues
    - Ensure proper import and definition of variables
    - Add instructions to install `libomp` for macOS to resolve XGBoost library loading error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,69 @@
-import streamlit as st
 import numpy as np
-import pandas as pd
-from time import time
+import shap
+import streamlit as st
+import xgboost
+from sklearn.model_selection import train_test_split
+from streamlit_shap import st_shap
 
-st.title('st.cache')
+st.set_page_config(layout="wide")
 
-# Not using cache
-a0 = time()
-st.subheader('Not using st.cache')
+@st.cache_data
+def load_data():
+    return shap.datasets.adult()
 
-@st.cache(suppress_st_warning=True)
-def load_data_a():
-    df = pd.DataFrame(
-        np.random.rand(2000000, 5),
-        columns=['a', 'b', 'c', 'd', 'e']
-    )
-    for i in range(200):
-        np.random.rand(2000000, 5)
-    return df
+@st.cache_data
+def load_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+    d_train = xgboost.DMatrix(X_train, label=y_train)
+    d_test = xgboost.DMatrix(X_test, label=y_test)
+    params = {
+        "eta": 0.01,
+        "objective": "binary:logistic",
+        "subsample": 0.5,
+        "base_score": np.mean(y_train),
+        "eval_metric": "logloss",
+        "n_jobs": -1,
+    }
+    model = xgboost.train(params, d_train, 10, evals = [(d_test, "test")], verbose_eval=100, early_stopping_rounds=20)
+    return model
 
-def call_and_measure():
-    start = time()
-    st.write(load_data_a())
-    end = time()
-    st.info(end - start)
+st.title("`streamlit-shap` for displaying SHAP plots in a Streamlit app")
 
-call_and_measure()
-call_and_measure()
+with st.expander('About the app'):
+    st.markdown('''[`streamlit-shap`](https://github.com/snehankekre/streamlit-shap) is a Streamlit component that provides a wrapper to display [SHAP](https://github.com/slundberg/shap) plots in [Streamlit](https://streamlit.io/). 
+                    The library is developed by our in-house staff [Snehan Kekre](https://github.com/snehankekre) who also maintains the [Streamlit Documentation](https://docs.streamlit.io/) website.
+                ''')
 
-def next_page():
-    st.query_params['challenge'] = 'Day25'
+st.header('Input data')
+X,y = load_data()
+X_display,y_display = shap.datasets.adult(display=True)
 
-st.button('Next', on_click=next_page)
+with st.expander('About the data'):
+    st.write('Adult census data is used as the example dataset.')
+with st.expander('X'):
+    st.dataframe(X)
+with st.expander('y'):
+    st.dataframe(y)
+
+st.header('SHAP output')
+
+# train XGBoost model
+model = load_model(X, y)
+
+# compute SHAP values
+explainer = shap.Explainer(model, X)
+shap_values = explainer(X)
+
+with st.expander('Waterfall plot'):
+    st_shap(shap.plots.waterfall(shap_values[0]), height=300)
+with st.expander('Beeswarm plot'):
+    st_shap(shap.plots.beeswarm(shap_values), height=300)
+
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+with st.expander('Force plot'):
+    st.subheader('First data instance')
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
+    st.subheader('First thousand data instance')
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
